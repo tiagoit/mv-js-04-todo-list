4788b954ca554b6bb4b13efdb9483868d5a5bd86
AndresFMoya <andresfmoya@gmail.com>
2019-12-19 11:10:12 -0500

    style: add description to todos view

--- src/views/todo.view.js
+++ src/views/todo.view.js
@@ -13,14 +13,36 @@ class TodoView {
       <li>
         <div class="d-flex">
           <input type="checkbox" value="" />
-          <div class="btn btn-light" data-toggle="collapse" href="#${id}" role="button">${todo.title}</div>
+          <a class="btn btn-light" data-toggle="collapse" href="#${id}" role="button">${todo.title}</a>
         </div>
         <div class="collapse" id="${id}">
           <div class="card card-body">
-            Description: ${todo.description}
-            Due date: ${todo.dueDate && todo.dueDate.toISOString().slice(0, 10)}
-            Done date: ${todo.doneDate} 
-            Priority: ${todo.priority}
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text">Description</span>
+              </div>
+              <textarea class="form-control" aria-label="With textarea">${todo.description}</textarea>
+            </div>
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text">Due Date</span>
+              </div>
+              <textarea class="form-control" aria-label="With textarea">${todo.dueDate && todo.dueDate.toISOString().slice(0, 10)}</textarea>
+            </div>  
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text">Done date</span>
+              </div>
+              <textarea class="form-control" aria-label="With textarea">${todo.doneDate}</textarea>
+            </div>  
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text">Priority</span>
+              </div>
+              <textarea class="form-control" aria-label="With textarea">${todo.priority}</textarea>
+            </div>
+            </div>
+            <button class="btn-success w-100">Save</button>
           </div>
         </div>
       </li>
